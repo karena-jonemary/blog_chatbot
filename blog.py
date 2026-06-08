daf4385cfdbdf7643af02922c2243ac0e6631923
KARENA JONEMARY J <927623bcs042@mkce.ac.in>
2026-06-08 14:17:30 +0530

    Add files via upload

--- blog.py
+++ blog.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 import streamlit as st
-genai.configure(api_key="AIzaSyAiMmvF64z29gfkBAUtbMMlOC2zGt0tY6M")
+genai.configure(api_key="YOUR_API_KEY_HERE")
 model=genai.GenerativeModel("gemini-2.5-flash")
 st.title("JonieBlog!")
 prompt=st.text_input("Can you write a blog post")
